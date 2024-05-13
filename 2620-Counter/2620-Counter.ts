94a4a8dac8d1b1b6acb98181b9883c05f7e5aa1f
Pedro Ferrari <pedrosantanaferrari96@gmail.com>
2024-05-13 14:41:24 -0300

    Update 2620-Counter.ts

--- 2620-Counter/2620-Counter.ts
+++ 2620-Counter/2620-Counter.ts
@@ -1,12 +1,15 @@
-     n = n + 1
-     return result
-    }
+function createCounter(n: number): () => number {
+    
+    return function() {
+    const result = n
+     n = n + 1
+     return result
+    }
 }
 
 
- 
- const counter = createCounter(10)
- counter() // 10
- counter() // 11
- counter() // 12
-10
+ 
+ const counter = createCounter(10)
+ counter() // 10
+ counter() // 11
+ counter() // 12
