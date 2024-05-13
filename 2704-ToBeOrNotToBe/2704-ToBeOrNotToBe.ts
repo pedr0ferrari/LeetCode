051ca372b21d2b742c81ec6577e0d112809247ef
Pedro Ferrari <pedrosantanaferrari96@gmail.com>
2024-05-13 14:42:16 -0300

    Update 2704-ToBeOrNotToBe.ts

--- 2704-ToBeOrNotToBe/2704-ToBeOrNotToBe.ts
+++ 2704-ToBeOrNotToBe/2704-ToBeOrNotToBe.ts
@@ -1,16 +1,23 @@
-        notToBe: (val2) => {
-            if(val !== val2)  return true;
-            throw new Error ("Equal");
-            throw new Error ("Not Equal");
-        },
-    return {
-        toBe: (val2) => {
-            if(val === val2) return true;
-
-function expect(val: any): ToBeOrNotToBe {
-
-    toBe: (val: any) => boolean;
-    notToBe: (val: any) => boolean;
+type ToBeOrNotToBe = {
+    toBe: (val: any) => boolean;
+    notToBe: (val: any) => boolean;
 };
-type ToBeOrNotToBe = {
-() => expect(5).toBe(5)
+
+function expect(val: any): ToBeOrNotToBe {
+
+    return {
+        toBe: (val2) => {
+            if(val === val2) return true;
+            throw new Error ("Not Equal");
+        },
+        notToBe: (val2) => {
+            if(val !== val2)  return true;
+            throw new Error ("Equal");
+        }
+    }
+};
+
+
+
+//  expect(5).toBe(5); // true
+//  expect(5).notToBe(5); // throws "Equal"
