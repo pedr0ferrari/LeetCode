6ab54d180c7f1f90dfdb749b02d33c0a1b40a2ab
Pedro Ferrari <pedrosantanaferrari96@gmail.com>
2024-05-14 18:25:26 -0300

    Update 2665-CounterII.ts

--- 2665-CounterII/2665-CounterII.ts
+++ 2665-CounterII/2665-CounterII.ts
@@ -1,16 +1,29 @@
-        },
-        reset: () => {
-            return currentValue = init; 
-        },
-        },
-        decrement: () => {
-            return currentValue -= 1; 
-    return {
-        increment: () => {
-           return currentValue += 1; 
-     let currentValue = init; 
-
+type Counter = {
+    increment: () => number,
+    decrement: () => number,
+    reset: () => number,
 }
 
-function createCounter(init: number): Counter {
-5
+function createCounter(init: number): Counter {
+     let currentValue = init; 
+
+    return {
+        increment: () => {
+           return currentValue += 1; 
+        },
+        decrement: () => {
+            return currentValue -= 1; 
+        },
+        reset: () => {
+            return currentValue = init; 
+        },
+    }
+    
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
