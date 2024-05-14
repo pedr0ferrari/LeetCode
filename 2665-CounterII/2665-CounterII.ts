81dda4a5ff6ee9188447aa371fedf326ef14e90e
Pedro Ferrari <pedrosantanaferrari96@gmail.com>
2024-05-14 18:10:55 -0300

    Create file

---
+++ 2665-CounterII/2665-CounterII.ts
@@ -0,0 +1,16 @@
+        },
+        reset: () => {
+            return currentValue = init; 
+        },
+        },
+        decrement: () => {
+            return currentValue -= 1; 
+    return {
+        increment: () => {
+           return currentValue += 1; 
+     let currentValue = init; 
+
+}
+
+function createCounter(init: number): Counter {
+5
